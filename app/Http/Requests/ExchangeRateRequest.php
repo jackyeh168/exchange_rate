a6e27352ae0dd7c819492d5c70412c4680e8ed55
Jach Yeh <jack59074@gmail.com>
2022-01-04 22:31:27 +0800

    feat(validator): add validation for exchange rate api

---
+++ app/Http/Requests/ExchangeRateRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
+
+class ExchangeRateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $currency_types = $this->getCurrencyTypeList();
+        
+        return [
+            'from' => 'required|string|in:' . implode(',', $currency_types), // origin_currency
+            'to' => 'required|string',   // target currency
+            'price'=>'required'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $responseData = $validator->errors();
+        $response = response()->json($responseData, Response::HTTP_BAD_REQUEST);
+        throw new HttpResponseException($response);
+    }
+
+    private function getCurrencyTypeList()
+    {
+        return collect(config('exchange_rate.currencies'))->keys()->all();
+    }
+}
